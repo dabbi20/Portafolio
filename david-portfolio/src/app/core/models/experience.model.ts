98bf87a56aeac86ee1883b311f1a250e9034fb18
David Carrasco <davidack123456789@gmail.com>
2026-06-01 18:41:28 -0500

    feat: add professional experience data

---
+++ david-portfolio/src/app/core/models/experience.model.ts
@@ -0,0 +1,11 @@
+export interface Experience {
+  id: string;
+  company: string;
+  role: string;
+  modality: string;
+  startDate: string;
+  endDate: string;
+  technologies: string[];
+  responsibilities: string[];
+  current: boolean;
+}
