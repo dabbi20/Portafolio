5a908f30484bcdbe66b3c3bb90d7823584302702
David Carrasco <davidack123456789@gmail.com>
2026-06-02 12:05:54 -0500

    logos y docker

--- david-portfolio/src/app/core/services/theme.service.ts
+++ david-portfolio/src/app/core/services/theme.service.ts
@@ -26,5 +26,20 @@ export class ThemeService {
 
     document.documentElement.classList.toggle('light', theme === 'light');
     document.documentElement.classList.toggle('dark', theme === 'dark');
+
+    this.updateFavicon(theme);
+  }
+
+  private updateFavicon(theme: Theme) {
+    const favicon = document.querySelector(
+      "link[rel='icon']"
+    ) as HTMLLinkElement;
+
+    if (!favicon) return;
+
+    favicon.href =
+      theme === 'dark'
+        ? 'assets/logos/logo-square-dark-favicon.png'
+        : 'assets/logos/logo-square-light-favicon.png';
   }
 }
